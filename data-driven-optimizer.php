7d0ccc9320df4b2b94393c4d6123529503416257
Tavorick <lars.picke@gmail.com>
2026-02-16 23:04:35 +0100

    Update data-driven-optimizer.php

--- data-driven-optimizer.php
+++ data-driven-optimizer.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'DDO_PLUGIN_VERSION', '1.1.0' );
+define( 'DDO_PLUGIN_VERSION', '1.2.1' );
 define( 'DDO_PLUGIN_FILE', __FILE__ );
 define( 'DDO_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'DDO_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
